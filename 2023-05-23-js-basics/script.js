5862dc8417521e1d1490c27ebba25f9db0620f26
RomanasV <romanasvenckus@gmail.com>
2023-05-23 19:24:56 +0300

    first commit

---
+++ 2023-05-23-js-basics/script.js
@@ -0,0 +1,24 @@
+// Kintamojo sukūrimas:
+// 1. Iniciavimo žodelis (var, let arba const)
+// 2. Kintamojo pavadinimas
+// 3. Lygybės ženklas
+// 4. Kintamojo vertė
+
+var vardas = 'John'; // String (tekstas) duomenų tipas
+let pavarde = "Doe"; // String (tekstas) duomenų tipas
+let miestas = `Kaunas`; // String (tekstas) duomenų tipas
+let yraStudentas = false; // Boolean (true/false) duomenų tipas
+let amzius = 30; // Number (skaičius) duomenų tipas
+const asmensKodas = 345645646465; // Number (skaičius) duomenų tipas
+
+// typeof parodo kokio tipo duomenys tai yra
+console.log(vardas, typeof vardas);
+console.log(pavarde, typeof pavarde);
+console.log(miestas, typeof miestas);
+console.log(amzius, typeof amzius);
+console.log(yraStudentas, typeof yraStudentas);
+console.log(asmensKodas, typeof asmensKodas);
+
+amzius = 31;
+console.log(amzius);
+
