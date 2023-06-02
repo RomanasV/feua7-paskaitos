4e70cc5413bee9b638995c544bbcea35b0e2cd13
RomanasV <romanasvenckus@gmail.com>
2023-06-02 19:29:18 +0300

    fizz buzz task finished

--- 2023-05-31-loops/script.js
+++ 2023-05-31-loops/script.js
@@ -217,6 +217,94 @@ console.groupEnd();
 // } while (m < 0);
 
 
-for (let i = 1; i <= 100; i++) {
-  console.log(i);
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+
+// FIZZ BUZZ
+// PIRMAS BŪDAS
+// for (let i = 1; i <= 350; i++) {
+//   if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0 && i % 9 === 0) {
+//     console.log('FizzBuzzBiffFuzz');
+//   } else if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+//     console.log('FizzBuzzBiff');
+//   } else if (i % 3 === 0 && i % 5 === 0 && i % 9 === 0) {
+//     console.log('FizzBuzzFuzz');
+//   } else if (i % 3 === 0 && i % 7 === 0 && i % 9 === 0) {
+//     console.log('FizzBiffFuzz');
+//   } else if (i % 5 === 0 && i % 7 === 0 && i % 9 === 0) {
+//     console.log('BuzzBiffFuzz');
+//   } else if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//   } else if (i % 3 === 0 && i % 7 === 0) {
+//     console.log('FizzBiff');
+//   } else if (i % 3 === 0 && i % 9 === 0) {
+//     console.log('FizzFuzz');
+//   } else if (i % 5 === 0 && i % 7 === 0) {
+//     console.log('BuzzBiff');
+//   } else if (i % 5 === 0 && i % 9 === 0) {
+//     console.log('BuzzFuzz');
+//   } else if (i % 7 === 0 && i % 9 === 0) {
+//     console.log('BiffFuzz');
+//   } else if (i % 3 === 0) {
+//     console.log('Fizz');
+//   } else if (i % 5 === 0) {
+//     console.log('Buzz');
+//   } else if (i % 7 === 0) {
+//     console.log('Biff');
+//   } else if (i % 9 === 0) {
+//     console.log('Fuzz');
+//   } else {
+//     console.log(i)
+//   }
+// }
+
+
+// ANTRAS BŪDAS
+// for (let i = 1; i <= 350; i++) {
+//   let output = '';
+
+//   if (i % 3 === 0) {
+//     output += 'Fizz';
+//   }
+
+//   if (i % 5 === 0) {
+//     output += 'Buzz';
+//   }
+
+//   if (i % 7 === 0) {
+//     output += 'Biff';
+//   }
+
+//   if (i % 9 === 0) {
+//     output += 'Fuzz';
+//   }
+
+//   if (i % 11 === 0) {
+//     output += 'Bizz';
+//   }
+
+//   if (!output) {
+//     output = i;
+//   }
+
+//   console.log(output);
+// }
+
+
+// TREČIAS BŪDAS
+for (let i = 1; i <= 350; i++) {
+  let output = '';
+
+  if (i % 3 === 0) output += 'Fizz';
+  if (i % 5 === 0) output += 'Buzz';
+  if (i % 7 === 0) output += 'Biff';
+  if (i % 9 === 0) output += 'Fuzz';
+  if (i % 11 === 0) output += 'Bizz';
+  if (i % 13 === 0) output += 'Buff';
+
+  if (!output) output = i;
+
+  console.log(output);
 }
