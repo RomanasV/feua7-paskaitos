ca060f27c5a6ca939065408db2bf27aefcd1aee2
RomanasV <romanasvenckus@gmail.com>
2023-06-13 19:58:41 +0300

    slice task

--- 2023-06-12-arrays/script.js
+++ 2023-06-12-arrays/script.js
@@ -1,3 +1,5 @@
+console.groupCollapsed('Array basics');
+
 // Masyvas / Array
 let numsArray = [5, 10, 15, 20];
 // Index         0   1   2   3
@@ -144,3 +146,126 @@ let splicedNums = nums.splice(2, 0, 2.5, 2.7, 2.8);
 console.log(splicedNums);
 
 console.log(nums);
+
+console.groupEnd();
+
+
+// UZDUOTIS dirbant su pirkinių sąrašu (bent 7 pirkiniai):
+let shoppingList = ['vynuogės', 'alyvuogės', 'mandarinai', 'uobuoliai', 'pomidorai', 'šprotai', 'bananai', 'gervuogės'];
+// Kiekvienam iš punktų sukurti po atskirą kintamąjį ir jame išsaugoti reikšmę. Šią reikšmę atvaizduoti konsolėje.
+
+
+// 1. Išimti pirmus du pirkinius.
+let shoppingListTask1 = shoppingList.slice(0, 2);
+// console.log(shoppingListTask1);
+
+// let slicedShoppingList = shoppingList.splice(0, 2);
+// console.log(slicedShoppingList);
+
+let toSplicedShoppingList1 = shoppingList.toSpliced(2);
+// console.log(toSplicedShoppingList1);
+
+
+// 2. Išimti tris paskutinius pirkinius.
+let shoppingListTask2 = shoppingList.slice(-3);
+// console.log(shoppingListTask2);
+
+// let slicedShoppingList = shoppingList.splice(-3);
+// console.log(slicedShoppingList);
+
+// let num = shoppingList.length - 3
+// let toSplicedShoppingList2 = shoppingList.toSpliced(num, 3);
+// console.log(toSplicedShoppingList2);
+
+
+// 3. Išimti ketvirtą ir penktą pirkinį.
+let shoppingListTask3 = shoppingList.slice(3, 5);
+// console.log(shoppingListTask3);
+
+// let slicedShoppingList = shoppingList.splice(3, 2);
+// console.log(slicedShoppingList);
+
+
+
+// 4. Išimti antrą ir trečią pirkinį.
+let shoppingListTask4 = shoppingList.slice(1, 3);
+// console.log(shoppingListTask4);
+
+// let slicedShoppingList = shoppingList.splice(1, 2);
+// console.log(slicedShoppingList);
+
+
+
+
+// 5. Išimti visus, išskyrus pirmą pirkinį.
+let shoppingListTask5 = shoppingList.slice(1);
+// console.log(shoppingListTask5);
+
+// let slicedShoppingList = shoppingList.splice(1);
+// console.log(slicedShoppingList);
+
+// console.log(shoppingList);
+
+
+
+let moviesList = ['nitroKids', 'turbo', 'minions', 'gruobuonis', 'vėžliukaiNindzės', 'dvigubasSmūgis', 'kruvinasSportas', 'warCraft', 'pokemonai', 'supermenas'];
+// Sukurti 10-ties filmų masyvą ir iš jo:
+// 2.1. Išimti pirmus 5 filmus.
+let moviesTask1 = moviesList.slice(0, 5);
+console.log(moviesTask1);
+
+// 2.2. Išimti 4 paskutinius filmus.
+let moviesTask2 = moviesList.slice(-4);
+console.log(moviesTask2);
+
+// 2.3. Išimti nuo trečio iki septinto filmo.
+let moviesTask3 = moviesList.slice(2, 7);
+console.log(moviesTask3);
+
+// 2.4. Išimti antrą ir trečią filmą.
+let moviesTask4 = moviesList.slice(1, 3);
+console.log(moviesTask4);
+
+// 2.5. Išimti visus, išskyrus pirmą filmą.
+let moviesTask5 = moviesList.slice(1);
+console.log(moviesTask5);
+
+// 2.6. Išimti 5 filmus skaičiuojant nuo trečio.
+let moviesTask6 = moviesList.slice(2, 7);
+console.log(moviesTask6);
+
+// 2.7. Išimti tris filmus nuo galo, tačiau palikti paskutinį.
+let moviesTask7 = moviesList.slice(-4, -1);
+console.log(moviesTask7);
+
+
+
+// 2.8. Vietoje penkto filmo įrašyti du naujus filmus.
+moviesList.splice(4, 1, 'Filmas 1', 'Filmas 2');
+console.log(moviesList);
+
+// 2.9. Pašalinti pirmą filmą.
+// moviesList.splice(0, 1);
+moviesList.shift();
+console.log(moviesList);
+
+// 2.10. Įrašyti naują filmą į masyvo pradžią.
+// moviesList.unshift('Filmas 3');
+moviesList.splice(0, 0, 'Filmas 3');
+console.log(moviesList);
+
+
+// 11.1. Išimti pirmus tris filmus, skaičiuojant nuo antro filmo.
+let moviesTask111 = moviesList.slice(1, 4);
+// 11.2. Išimti paskutinius tris filmus, skaičiuojant nuo priešpaskutinio filmo.
+let moviesTask112 = moviesList.slice(-4, -1);
+
+// 11.3. Šiuos išimtus filmus išsaugoti naujame masyve ir padaryti, jog jo nariai būtų apsukti.
+console.log(moviesTask111);
+console.log(moviesTask112);
+
+// let moviesList113 = [moviesTask111, moviesTask112].flat().reverse();
+// let moviesList113 = [...moviesTask111, ...moviesTask112].reverse();
+let moviesList113 = moviesTask111.concat(moviesTask112).reverse();
+
+console.log(moviesList113);
