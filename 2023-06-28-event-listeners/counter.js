b0d05704123c7c96652777b62b1d7181acd23907
RomanasV <romanasvenckus@gmail.com>
2023-06-29 19:30:36 +0300

    counter task

--- 2023-06-28-event-listeners/counter.js
+++ 2023-06-28-event-listeners/counter.js
@@ -9,14 +9,26 @@ let numbers = document.querySelector('#numbers');
 let numberDisplay = document.createElement('h3');
 let plusButton = document.createElement('button');
 let minusButton = document.createElement('button');
+let resetButton = document.createElement('button');
+let input = document.createElement('input');
 
-let num = 5;
+let initialNum = 5;
+
+let num = initialNum;
 
 numberDisplay.textContent = num;
 plusButton.textContent = '+';
 minusButton.textContent = '-';
+resetButton.textContent = 'Reset';
 
-numbers.append(numberDisplay, minusButton, plusButton);
+input.type = 'number';
+input.max = 10;
+input.min = 1;
+input.value = num;
+
+changeColor();
+
+numbers.append(numberDisplay, input, minusButton, plusButton, resetButton);
 
 // 5. Sukurti „click" (paspaudimo) event listener'ius abiems mygtukams.
 // 6. „-" mygtuko event listeneris turėtų iškviesti funkciją, kuri sumažina skaičių h3 elemente, o „+" mygtuko paspaudimas turėtų skaičių padidinti
@@ -26,6 +38,7 @@ plusButton.addEventListener('click', () => {
   // num = num + 1;
   num++;
   numberDisplay.textContent = num;
+  input.value = num;
 
   if (num > 9) {
     plusButton.setAttribute('disabled', true);
@@ -34,12 +47,15 @@ plusButton.addEventListener('click', () => {
   if (num >= 2) {
     minusButton.removeAttribute('disabled');
   }
+
+  changeColor();
 })
 
 minusButton.addEventListener('click', () => {
   // num = num - 1;
   num--;
   numberDisplay.textContent = num;
+  input.value = num;
 
   if (num <= 9) {
     plusButton.removeAttribute('disabled');
@@ -48,4 +64,41 @@ minusButton.addEventListener('click', () => {
   if (num < 2) {
     minusButton.setAttribute('disabled', true);
   }
+
+  changeColor();
 })
+
+resetButton.addEventListener('click', () => {
+  num = initialNum;
+  numberDisplay.textContent = num;
+  input.value = num;
+  plusButton.removeAttribute('disabled');
+  minusButton.removeAttribute('disabled');
+  changeColor();
+})
+
+input.addEventListener('input', () => {
+  num = input.value;
+  numberDisplay.textContent = num;
+  changeColor();
+
+  if (num <= 9) {
+    plusButton.removeAttribute('disabled');
+  } else {
+    plusButton.setAttribute('disabled', true);
+  }
+
+  if (num >= 2) {
+    minusButton.removeAttribute('disabled');
+  } else {
+    minusButton.setAttribute('disabled', true);
+  }
+})
+
+function changeColor() {
+  if (num >= 5) {
+    numberDisplay.style.color = 'green';
+  } else {
+    numberDisplay.style.color = 'red';
+  }
+}
